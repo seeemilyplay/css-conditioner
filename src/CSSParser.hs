16615c58ac5983e5d95d427e100cac16590f17a2
Emily <emily@skrivapa.se>
2012-04-19 23:19:31 +0200

    improve the css parser by adding in lexing (comments work now, amongst other things)

--- src/CSSParser.hs
+++ src/CSSParser.hs
@@ -1,9 +1,16 @@
+{- |
+    An incomplete css parser which pretty much follows this
+    http://www.w3.org/TR/CSS21/grammar.html
+-}
 module CSSParser where
 
 import Control.Applicative ((<$>))
 import Data.Char
+import Data.Functor.Identity
 import Data.Maybe (listToMaybe)
 import Text.ParserCombinators.Parsec
+import Text.Parsec.Language
+import Text.Parsec.Token
 
 data RuleSet = RuleSet [Selector] [Declaration]
   deriving Show
@@ -18,51 +25,67 @@ data SelectorTerm = NamedElement String
                     | Pseudo String
   deriving Show
 
-data Declaration = Declaration Property [Term]
+data Declaration = Declaration Property [Term] (Maybe Priority)
   deriving Show
 
 data Property = Property String
   deriving Show
 
-data Term =   NumberTerm Double
-            | Percentage Double
-            | PixelLength Double
-            | PointLength Double
-            | Ems Double
+data Priority = Priority String
+  deriving Show
+
+data Term =   NumberTerm Double (Maybe Unit)
             | StringTerm String
             | IdentTerm String
             | URI String
             | RGBColor Integer Integer Integer
+            | FunctionTerm String [Term]
+  deriving Show
+
+data Unit = Percentage
+          | Pixel
+          | Centimeter
+          | Millimeter
+          | Inch
+          | Point
+          | Pica
+          | Ems
+          | Exs
+          | Degree
+          | Radian
+          | Grad
+          | Millisecond
+          | Second
+          | Hertz
+          | Kilohertz
   deriving Show
 
 stylesheet :: GenParser Char st [RuleSet]
-stylesheet = many ruleset
+stylesheet = do
+  (whiteSpace lexer)
+  many ruleset
 
 ruleset :: GenParser Char st RuleSet
 ruleset = do
-  spaces
   sels <- selectors
-  spaces
-  _ <- char '{'
-  spaces
-  decs <- declarations
-  spaces
-  _ <- char '}'
-  spaces
+  decs <- (braces lexer) $ declarations
   return $ RuleSet sels decs
 
 selectors :: GenParser Char st [Selector]
-selectors = try (sepBy1 selector comma) <|> endBy1 selector comma
-  where
-    comma = char ',' >> spaces
+selectors = do
+  s <- selector
+  mcomma <- optionMaybe (comma lexer)
+  case mcomma of
+    Just _ -> do
+      sels <- selectors
+      return $ s : sels
+    Nothing -> return [s]
 
 selector :: GenParser Char st Selector
-selector = Selector <$> (
-  try (sepBy1 simpleSelector spaces)
-  <|> endBy1 simpleSelector spaces)
+selector = Selector <$> many1 simpleSelector
 
 simpleSelector :: GenParser Char st [SelectorTerm]
-simpleSelector = choice [withElement, withoutElement]
+simpleSelector = (lexeme lexer) $ choice [withElement, withoutElement]
   where
     withElement = do
       el <- elementName
@@ -74,7 +97,7 @@ elementName :: GenParser Char st SelectorTerm
 elementName = choice [named, wildcard]
   where
     named = do
-      s <- identString
+      s <- map toLower <$> unlexemedIdentifier
       return $ NamedElement s
     wildcard = do
       _c <- char '*'
@@ -89,87 +112,199 @@ identity = do
 clazz :: GenParser Char st SelectorTerm
 clazz = do
   _ <- char '.'
-  s <- identString
+  s <- unlexemedIdentifier
   return $ Class s
 
 pseudo :: GenParser Char st SelectorTerm
 pseudo = do
   _ <- char ':'
-  s <- identString
+  s <- map toLower <$> unlexemedIdentifier
   return $ Pseudo s
 
+unlexemedIdentifier :: GenParser Char st String
+unlexemedIdentifier = do
+  start <- identStart cssDef
+  rest <- many $ identLetter cssDef
+  return $ start : rest
+
 declarations :: GenParser Char st [Declaration]
-declarations = try (sepBy declaration semicolon) <|> endBy declaration semicolon
-  where
-    semicolon = char ';' >> spaces
+declarations = do
+  mdec <- optionMaybe declaration
+  msemi <- optionMaybe (semi lexer)
+  case (mdec, msemi) of
+    (Just dec, Nothing) -> return [dec]
+    (Just dec, Just _) -> do
+      decs <- declarations
+      return $ dec : decs
+    (Nothing, Just _) -> declarations
+    (Nothing, Nothing) -> return []
 
 declaration :: GenParser Char st Declaration
 declaration = do
   prop <- property
-  spaces
-  _ <- char ':'
-  spaces
+  _ <- (colon lexer)
   terms <- expr
-  return $ Declaration prop terms
+  mprior <- optionMaybe priority
+  return $ Declaration prop terms mprior
 
 property :: GenParser Char st Property
-property = do
-  s <- identString
-  return $ Property s
+property = Property . map toLower <$> identifier lexer
+
+priority :: GenParser Char st Priority
+priority = do
+  _ <- char '!'
+  Priority . map toLower <$> (identifier lexer)
 
 expr :: GenParser Char st [Term]
-expr = sepBy1 term (optional operator >> spaces)
+expr = sepBy1 term (optional op)
 
-operator :: GenParser Char st Char
-operator = char ','
+op :: GenParser Char st String
+op = comma lexer <|> forwardSlash
+  where
+    forwardSlash = (lexeme lexer) $ do
+      c <- char '/'
+      return [c]
 
 term :: GenParser Char st Term
 term = choice
   [ numericTerm
   , stringTerm
-  , try uri <|> identTerm
+  , try uri <|> (try functionTerm <|> identTerm)
+  , try functionTerm <|> identTerm
   , identTerm
   , hexcolor ]
 
 numericTerm :: GenParser Char st Term
 numericTerm = do
   n <- num
-  try (choice [ percentage n
-              , try (pixelLength n) <|> pointLength n
-              , ems n ]) <|> number n
-  where
-    percentage n = do
-      _ <- char '%'
-      return $ Percentage n
-    pixelLength n = do
-      _ <- string "px"
-      return $ PixelLength n
-    pointLength n = do
-      _ <- string "pt"
-      return $ PointLength n
-    ems n = do
-      _ <- string "em"
-      return $ Ems n
-    number n = return $ NumberTerm n
+  munit <- optionMaybe unit
+  return $ NumberTerm n munit
+
+unit :: GenParser Char st Unit
+unit = choice
+  [ percentage
+  , centimeter
+  , inch
+  , degree
+  , radian
+  , grad
+  , second
+  , hertz
+  , kilohertz
+  , try pixel <|> (try point <|> pica)
+  , try millimeter <|> millisecond
+  ]
+
+percentage :: GenParser Char st Unit
+percentage = (lexeme lexer) $ do
+  _ <- char '%'
+  return Percentage
+
+centimeter :: GenParser Char st Unit
+centimeter = (lexeme lexer) $ do
+  _ <- stringIgnoreCase "cm"
+  return Centimeter
+
+inch :: GenParser Char st Unit
+inch = (lexeme lexer) $ do
+  _ <- stringIgnoreCase "in"
+  return Inch
+
+degree :: GenParser Char st Unit
+degree = (lexeme lexer) $ do
+  _ <- stringIgnoreCase "deg"
+  return Degree
+
+radian :: GenParser Char st Unit
+radian = (lexeme lexer) $ do
+  _ <- stringIgnoreCase "rad"
+  return Radian
+
+grad :: GenParser Char st Unit
+grad = (lexeme lexer) $ do
+  _ <- stringIgnoreCase "grad"
+  return Grad
+
+second :: GenParser Char st Unit
+second = (lexeme lexer) $ do
+  _ <- char 's'
+  return Second
+
+hertz :: GenParser Char st Unit
+hertz = (lexeme lexer) $ do
+  _ <- stringIgnoreCase "hz"
+  return Hertz
+
+kilohertz :: GenParser Char st Unit
+kilohertz = (lexeme lexer) $ do
+  _ <- stringIgnoreCase "khz"
+  return Kilohertz
+
+pixel :: GenParser Char st Unit
+pixel = (lexeme lexer) $ do
+  _ <- stringIgnoreCase "px"
+  return Pixel
+
+point :: GenParser Char st Unit
+point = (lexeme lexer) $ do
+  _ <- stringIgnoreCase "pt"
+  return Point
+
+pica :: GenParser Char st Unit
+pica = (lexeme lexer) $ do
+  _ <- stringIgnoreCase "pc"
+  return Pica
+
+millimeter :: GenParser Char st Unit
+millimeter = (lexeme lexer) $ do
+  _ <- stringIgnoreCase "mm"
+  return Millimeter
+
+millisecond :: GenParser Char st Unit
+millisecond = (lexeme lexer) $ do
+  _ <- stringIgnoreCase "ms"
+  return Millisecond
 
 stringTerm :: GenParser Char st Term
-stringTerm = StringTerm <$> choice [singleQuoted, doubleQuoted]
+stringTerm = StringTerm <$> quotedString
+
+identTerm :: GenParser Char st Term
+identTerm = do
+  s <- (identifier lexer)
+  return . IdentTerm $ map toLower s
+
+uri :: GenParser Char st Term
+uri = URI <$> do
+  _ <- stringIgnoreCase "url"
+  (parens lexer) url
+  where
+    url = quotedString <|> unquotedString
+    unquotedString = (lexeme lexer) $ manyTill anyChar ending
+    ending = lookAhead $ do
+      (whiteSpace lexer)
+      char ')'
+
+stringIgnoreCase :: String -> GenParser Char st String
+stringIgnoreCase [] = return ""
+stringIgnoreCase (c:cs) = do
+  x <- charIgnoreCase c
+  xs <- stringIgnoreCase cs
+  return $ x : xs
+
+charIgnoreCase :: Char -> GenParser Char st Char
+charIgnoreCase c =
+  char (toUpper c) <|> char (toLower c)
+
+quotedString :: GenParser Char st String
+quotedString = (lexeme lexer) $ singleQuoted <|> doubleQuoted
   where
     singleQuoted =
       between (char '\'') (char '\'') (many1 $ noneOf "\n\r\f\\\'")
     doubleQuoted =
       between (char '"') (char '"') (many1 $ noneOf "\n\r\f\\\"")
 
-identTerm :: GenParser Char st Term
-identTerm = do
-  s <- identString
-  return $ IdentTerm s
-
-uri :: GenParser Char st Term
-uri = URI <$> between (string "url(") (char ')') (many1 . satisfy $ (/=) ')')
-
 hexcolor :: GenParser Char st Term
-hexcolor = do
+hexcolor = (lexeme lexer) $ do
   _ <- char '#'
   try hexcolor6digits <|> hexcolor3digits
 
@@ -211,9 +346,15 @@ hexdigit = toInt . toLower <$> hexDigit
     toInt 'f' = 15
     toInt c = error $ "Internal parser error, expected valid hex char got " ++ [c]
 
+functionTerm :: GenParser Char st Term
+functionTerm = do
+  f <- map toLower <$> (identifier lexer)
+  args <- (parens lexer) $ expr
+  return $ FunctionTerm f args
+
 num :: GenParser Char st Double
 num = do
-  c <- option ' ' (char '-')
+  c <- option '+' (char '-' <|> char '+')
   let power = if c == '-' then (-1) else 1
   (*) power <$> (try asDecimal <|> asWhole)
   where
@@ -232,15 +373,23 @@ num = do
     maybeRead :: Read a => String -> Maybe a
     maybeRead = fmap fst . listToMaybe . reads
 
-identString :: GenParser Char st String
-identString = do
-  c <- choice [char '_', letter]
-  rest <- many $ choice [oneOf "_-", alphaNum]
-  return $ c : rest
+lexer :: GenTokenParser String st Identity
+lexer = makeTokenParser cssDef
+
+cssDef =
+  emptyDef
+    { commentStart = "/*"
+    , commentEnd = "*/"
+    , commentLine = ""
+    , nestedComments = False
+    , identStart = letter <|> char '_'
+    , identLetter = alphaNum <|> oneOf "_-"
+    , caseSensitive = False
+    }
 
 tryout :: FilePath -> IO ()
 tryout file = do
   csscontents <- readFile file
-  let css = parse stylesheet "Parse Error" csscontents
+  let css = parse stylesheet file csscontents
   print css
   return ()
